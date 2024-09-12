2b77ba5a2612c35fa0c02cb14fe2643aab51284f
ClementV <45317801+ClementV74@users.noreply.github.com>
2024-09-12 23:11:55 +0200

    Update ContentView.swift

--- anime/ContentView.swift
+++ anime/ContentView.swift
@@ -5,14 +5,22 @@ struct ContentView: View {
     @State private var isShowingLoadingScreen = true
     @State private var isShowingDownloadAlert = false
     @State private var isShowingLoginView = false
-    @State private var userId: Int? = nil // userId est initialisé à nil
-
+    
+    @State private var userId: Int? = {
+        let savedUserId = UserDefaults.standard.integer(forKey: "userId")
+        return savedUserId > 0 ? savedUserId : nil
+    }()
+    
+    
     var body: some View {
         NavigationView {
             ZStack {
-                WebView(url: URL(string: "https://anime-sama.fr")!, isShowingLoadingScreen: $isShowingLoadingScreen)
+                WebView(url: URL(string: "https://anime-sama.fr")!, 
+                    isShowingLoadingScreen: $isShowingLoadingScreen,
+                    userId: $userId) // Pass the Binding here
                     .navigationBarHidden(true)
                     .background(Color.black.edgesIgnoringSafeArea(.all))
+                   
                 
                 VStack {
                     Spacer()
@@ -208,6 +216,11 @@ struct ContentView: View {
 struct WebView: UIViewRepresentable {
     let url: URL
     @Binding var isShowingLoadingScreen: Bool
+    @Binding var userId: Int?
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
@@ -217,11 +230,7 @@ struct WebView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        // No need to implement anything here in this case
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
+        // Nous n'avons pas besoin de faire quelque chose ici pour ce cas précis
     }
 
     class Coordinator: NSObject, WKNavigationDelegate {
@@ -233,81 +242,242 @@ struct WebView: UIViewRepresentable {
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             parent.isShowingLoadingScreen = false
-            
-            // JavaScript code to inject styles and remove specific elements
+
+            // Assurez-vous que userId est correctement récupéré
+            let userId = parent.userId ?? 0
+            print("User ID in JavaScript: \(userId)")
+
             let jsCode = """
-                 const color = '#000411';
-                 const newStyle = `
-                     body {
-                         background-color: ${color} !important;
-                         background-image: none !important;
-                     }
-                     [href*="https://youradexchange.com/"],
-                     [href*="//feltatchaiz.net/"],
-                     [src*="https://youradexchange.com/"] {
-                         display: none !important;
-                     }
-                     img[src*="https://cdn.statically.io/gh/Anime-Sama/IMG/img/autres/flag_pal.png"] {
-                         display: none !important;
-                     }
-                     #dl-banner-300x250 {
-                         display: none !important;
-                     }
-                     img.logo-circular {
-                         border-radius: 50%;
-                         transition: transform 0.5s ease;
-                     }
-                     img.logo-circular:hover {
-                         transform: rotate(360deg);
-                     }
-                     .xl\\:pl-60.pt-1.sm\\:inline-flex {
-                         display: none !important;
-                     }
-                     .items-center.justify-center.align-center {
-                         display: none !important;
-                     }
-                 `;
-                 function injectStyle() {
-                     const style = document.createElement('style');
-                     style.type = 'text/css';
-                     style.innerHTML = newStyle;
-                     document.head.appendChild(style);
-                 }
-                 function replaceLogo() {
-                     const logos = document.querySelectorAll('img[src*="https://cdn.statically.io/gh/Anime-Sama/IMG/img/autres/logo_banniere.png"]');
-                     logos.forEach((logo) => {
-                         logo.src = 'https://feegaffe.fr/logo.png';
-                         logo.classList.add('logo-circular');
-                     });
-                 }
-                 function observeLogo() {
-                     const observer = new MutationObserver((mutations) => {
-                         mutations.forEach((mutation) => {
-                             if (mutation.addedNodes.length > 0 || mutation.type === 'attributes') {
-                                 replaceLogo();
-                             }
-                         });
-                     });
-                     observer.observe(document.body, {
-                         childList: true,
-                         subtree: true,
-                         attributes: true
-                     });
-                 }
-                 injectStyle();
-                 observeLogo();
-                 const paypalLink = document.querySelector('a[href="https://www.paypal.com/donate/?hosted_button_id=3FBNLMGT3JAJ2"]');
-                 if (paypalLink) {
-                     paypalLink.remove();
-                 }
-                 """
-            
-            webView.evaluateJavaScript(jsCode, completionHandler: nil)
+                const color = '#000411';
+                const newStyle = `
+                    body {
+                        background-color: ${color} !important;
+                        background-image: none !important;
+                    }
+                    [href*="https://youradexchange.com/"],
+                    [href*="//feltatchaiz.net/"],
+                    [src*="https://youradexchange.com/"] {
+                        display: none !important;
+                    }
+                    img[src*="https://cdn.statically.io/gh/Anime-Sama/IMG/img/autres/flag_pal.png"] {
+                        display: none !important;
+                    }
+                    #dl-banner-300x250 {
+                        display: none !important;
+                    }
+                    img.logo-circular {
+                        border-radius: 50%;
+                        transition: transform 0.5s ease;
+                    }
+                    img.logo-circular:hover {
+                        transform: rotate(360deg);
+                    }
+                    .xl\\:pl-60.pt-1.sm\\:inline-flex {
+                        display: none !important;
+                    }
+                    .items-center.justify-center.align-center {
+                        display: none !important;
+                    }
+                `;
+
+                function injectStyle() {
+                    const style = document.createElement('style');
+                    style.type = 'text/css';
+                    style.innerHTML = newStyle;
+                    document.head.appendChild(style);
+                }
+
+                function replaceLogo() {
+                    const logos = document.querySelectorAll('img[src*="https://cdn.statically.io/gh/Anime-Sama/IMG/img/autres/logo_banniere.png"]');
+                    logos.forEach((logo) => {
+                        logo.src = 'https://feegaffe.fr/logo.png';
+                        logo.classList.add('logo-circular');
+                    });
+                }
+
+                function observeLogo() {
+                    const observer = new MutationObserver((mutations) => {
+                        mutations.forEach((mutation) => {
+                            if (mutation.addedNodes.length > 0 || mutation.type === 'attributes') {
+                                replaceLogo();
+                            }
+                        });
+                    });
+                    observer.observe(document.body, {
+                        childList: true,
+                        subtree: true,
+                        attributes: true
+                    });
+                }
+
+                function refuseCookies() {
+                    const rejectButton = document.querySelector('.qc-cmp2-close-icon');
+                    if (rejectButton) {
+                        rejectButton.click();
+                        console.log('Cookies refusés automatiquement');
+                    } else {
+                        console.log('Bouton de refus des cookies non trouvé');
+                    }
+                }
+
+                function observeConsentPopup() {
+                    const observer = new MutationObserver((mutations) => {
+                        mutations.forEach((mutation) => {
+                            if (mutation.addedNodes.length > 0) {
+                                refuseCookies();
+                            }
+                        });
+                    });
+
+                    observer.observe(document.body, {
+                        childList: true,
+                        subtree: true
+                    });
+                }
+
+                injectStyle();
+                observeLogo();
+                observeConsentPopup();
+
+                const paypalLink = document.querySelector('a[href="https://www.paypal.com/donate/?hosted_button_id=3FBNLMGT3JAJ2"]');
+                if (paypalLink) {
+                    paypalLink.remove();
+                }
+            """
+
+            let jsCodeWithUserId = """
+                \(jsCode)
+                function injectButton(userId) {
+                    const pElement = document.querySelector('p.text-xs.text-white.font-base.mt-1');
+                    if (pElement) {
+                        const button = document.createElement('button');
+                        button.textContent = "Charger l'historique enregistré";
+                        button.style.backgroundColor = 'rgb(52, 152, 219)';
+                        button.style.color = 'rgb(255, 255, 255)';
+                        button.style.padding = '10px 20px';
+                        button.style.border = 'none';
+                        button.style.borderRadius = '5px';
+                        button.style.cursor = 'pointer';
+                        button.addEventListener('click', async () => {
+                            try {
+                                // Récupérer les informations de la base de données avec l'ID utilisateur
+                                const response = await fetch('https://feegaffe.fr/getUserData.php?userId=' + userId);
+                                
+                                // Conserver la clé `userId`
+                                const userIdKey = 'userId';
+                                const userIdValue = localStorage.getItem(userIdKey);
+
+                                // Vider le localStorage
+                                localStorage.clear();
+
+                                // Restaurer la clé `userId`
+                                if (userIdValue) {
+                                    localStorage.setItem(userIdKey, userIdValue);
+                                }
+                                
+                                if (!response.ok) {
+                                    throw new Error('Network response was not ok');
+                                }
+                                
+                                const data = await response.json();
+                                console.log('Données récupérées :', data);
+
+                                // Traiter les données JSON
+                                for (const key in data) {
+                                    if (data.hasOwnProperty(key)) {
+                                        const value = data[key];
+                                        // Si la valeur est un tableau
+                                        if (Array.isArray(value)) {
+                                            // Stocker les données dans le local storage
+                                            localStorage.setItem(key, JSON.stringify(value));
+                                            
+                                            // Traitement spécifique pour histoUrl, histoEp, et histoName
+                                            if (key === 'histoEp') {
+                                                const histoUrl = data.histoUrl || [];
+                                                const histoEp = value;
+                                                
+                                                // Assurez-vous que histoUrl et histoEp ont la même longueur
+                                                if (histoUrl.length !== histoEp.length) {
+                                                    console.error('Les longueurs de histoUrl et histoEp ne correspondent pas.');
+                                                    return;
+                                                }
+
+                                                // Fonction pour convertir "Épisode X" en numéro d'épisode
+                                                function convertEpisodeNumber(episodeString) {
+                                                    let cleanedString = episodeString.replace("Episode ", "");
+                                                    // Vérifier si c'est un nombre valide
+                                                    let episodeNumber = parseInt(cleanedString, 10);
+                                                    // Soustraire 1 pour obtenir l'index basé sur 0
+                                                    return !isNaN(episodeNumber) ? episodeNumber - 1 : null;
+                                                }
+
+                                                // Parcourir les URLs et les épisodes correspondants
+                                                histoUrl.forEach((url, index) => {
+                                                    const episodeString = histoEp[index];
+                                                    const episodeNumber = convertEpisodeNumber(episodeString); // Convertir l'épisode
+                                                    if (episodeNumber !== null) {
+                                                        const key = `savedEpNb${url}`; // Clé avec l'URL formatée
+                                                        localStorage.setItem(key, episodeNumber);
+                                                        console.log(`Stocké dans localStorage : ${key} = ${episodeNumber}`);
+                                                    } else {
+                                                        console.warn(`Numéro d'épisode invalide pour l'URL : ${url} avec la chaîne : ${episodeString}`);
+                                                    }
+                                                });
+                                            }
+
+                                            if (key === 'histoName') {
+                                                const histoUrl = data.histoUrl || [];
+                                                const histoName = value;
+
+                                                // Assurez-vous que histoUrl et histoName ont la même longueur
+                                                if (histoUrl.length !== histoName.length) {
+                                                    console.error('Les longueurs de histoUrl et histoName ne correspondent pas.');
+                                                    return;
+                                                }
+
+                                                // Parcourir les URLs et les noms des épisodes correspondants
+                                                histoUrl.forEach((url, index) => {
+                                                    const episodeName = histoName[index];
+                                                    const key = `savedEpName${url}`; // Clé avec l'URL formatée
+                                                    localStorage.setItem(key, episodeName);
+                                                    console.log(`Stocké dans localStorage : ${key} = ${episodeName}`);
+                                                });
+                                            }
+
+                                        } else {
+                                            // Stocker les données dans le local storage
+                                            localStorage.setItem(key, JSON.stringify(value));
+                                        }
+                                    }
+                                }
+                                
+                                // Recharger la page après avoir traité les données
+                                location.reload();
+                                
+                            } catch (error) {
+                                console.error('Erreur lors de la récupération des données :', error);
+                                pElement.innerHTML = 'Erreur lors de la récupération des données : ' + error.message;
+                            }
+                        });
+                        pElement.parentNode.insertBefore(button, pElement.nextSibling);
+                    } else {
+                        setTimeout(() => injectButton(userId), 1000);
+                    }
+                }
+                injectButton(\(userId));
+            """
+
+
+            webView.evaluateJavaScript(jsCodeWithUserId) { result, error in
+                if let error = error {
+                    print("JavaScript evaluation error: \(error.localizedDescription)")
+                } else {
+                    print("JavaScript evaluation result: \(String(describing: result))")
+                }
+            }
         }
     }
 }
-
-
 
 struct LoginView: View {
     @Binding var isShowing: Bool
@@ -394,6 +564,7 @@ struct LoginView: View {
                 if result.success, let userId = result.userId {
                     DispatchQueue.main.async {
                         self.userId = userId
+                        UserDefaults.standard.set(userId, forKey: "userId")
                         self.isShowing = false
                     }
                 } else {
@@ -416,7 +587,7 @@ struct LoginView: View {
 struct AuthenticationResult: Codable {
     let success: Bool
     let userId: Int? // Assurez-vous que c'est de type Int?
-    let message: String?
+    let message: String? //
 }
 
 extension Data {
